3938578876e1dd1f6888c9f09ab9b1a06eba82c3
ndangohmofor <ndangohmofor@gmail.com>
2023-05-21 22:33:58 -0400

    Added test cases for the auth and feed controllers

--- test/feed-controller.js
+++ test/feed-controller.js
@@ -1,7 +1,8 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const FeedController = require("../controllers/feed");
 const User = require("../models/user");
-const AuthController = require("../controllers/auth");
+const Post = require("../models/posts");
 const mongoose = require("mongoose");
 
 const {
@@ -9,11 +10,12 @@ const {
   DB_PASSWORD,
   DB_HOSTNAME,
   DB_DOMAINNAME,
+  PORT,
 } = require("../config");
 
 const MONGODBURI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}.${DB_DOMAINNAME}/test-messages?retryWrites=true&w=majority`;
 
-describe("Auth Controller", function () {
+describe("Feed Controller", function () {
   before(function (done) {
     mongoose
       .connect(MONGODBURI)
@@ -32,47 +34,30 @@ describe("Auth Controller", function () {
       });
   });
 
-  it("should throw an error with code 500 if accessing the database fails", function (done) {
-    sinon.stub(User, "findOne");
-    User.findOne.throws();
-
+  it("should add a created post to the posts of the creator", function (done) {
     const req = {
       body: {
-        email: "test@email.com",
-        password: "password",
+        title: "Test Post",
+        content: "A Test Post",
       },
-    };
-
-    AuthController.login(req, {}, () => {}).then((result) => {
-      expect(result).to.be.an("error");
-      expect(result).to.have.property("statusCode", 500);
-      done();
-    });
-
-    user.findOne.restore();
-  });
-
-  it("should send a response with a valid user status for an existing user", function (done) {
-    const req = {
+      file: {
+        path: "abc",
+      },
       userId: "5c0f66b979af55031b34728a",
     };
     const res = {
-      statusCode: 500,
-      userStatus: null,
-      status: function (code) {
-        this.statusCode = code;
+      status: function () {
         return this;
       },
-      json: function (data) {
-        this.userStatus = data.status;
-      },
+      json: function () {},
     };
-    AuthController.getUserStatus(req, res, () => {}).then(() => {
-      expect(res.statusCode).to.be.equal(200);
-      expect(res.userStatus).to.be.equal("I am new!");
+    FeedController.createPost(req, res, () => {}).then((savedUser) => {
+      expect(savedUser).to.have.property("posts");
+      expect(savedUser.posts).to.have.length(1);
       done();
     });
   });
+
   after(function (done) {
     User.deleteMany({})
       .then(() => {
